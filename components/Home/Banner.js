b1bb12dc57c0a37f746e39c33e527d958d7f603b
Panagiotis Apostolidis <apostolidhs@outlook.com>
2021-08-14 17:40:29 +0300

    telegram links update

--- components/Home/Banner.js
+++ components/Home/Banner.js
@@ -12,7 +12,7 @@ const Banner = () => {
         <p>Empowering waste producing and waste management companies like never before</p>
         <div className='buttons'>
           <p className='buy' onClick={() => window.open('https://pancakeswap.finance/')}>Buy $WASTE <i className='icon solid fa-shopping-cart'></i></p>
-          <p className='telegram' onClick={() => window.open('https://t.me/joinchat/yxQIw0366xUyODBk')}>Join Telegram <i className='icon brands fa-telegram-plane'></i></p>
+          <p className='telegram' onClick={() => window.open('https://t.me/wastebridge')}>Join Telegram <i className='icon brands fa-telegram-plane'></i></p>
         </div>
       </div>
     </section>
