5bd0ac2954ca662a80af9e084e1aeac3b7203456
Panagiotis Apostolidis <apostolidhs@outlook.com>
2021-08-14 17:36:29 +0300

    added logo + buy button

--- components/Home/Banner.js
+++ components/Home/Banner.js
@@ -4,10 +4,16 @@ const Banner = () => {
   return (
     <section id='banner'>
       <div className='inner'>
-        <div className='logo'><span className='icon fa-gem'></span></div>
+        {/* <div className='logo'><span className='icon fa-gem'></span></div> */}
+        <div className='logo'>
+          <img src='/logoWhite.jpg' alt='' />
+        </div>
         <h2>WasteBridge Protocol</h2>
         <p>Empowering waste producing and waste management companies like never before</p>
-        <p className='telegram' onClick={() => window.open('https://t.me/joinchat/yxQIw0366xUyODBk')}>Join Telegram <i className='icon brands fa-telegram-plane'></i></p>
+        <div className='buttons'>
+          <p className='buy' onClick={() => window.open('https://pancakeswap.finance/')}>Buy $WASTE <i className='icon solid fa-shopping-cart'></i></p>
+          <p className='telegram' onClick={() => window.open('https://t.me/joinchat/yxQIw0366xUyODBk')}>Join Telegram <i className='icon brands fa-telegram-plane'></i></p>
+        </div>
       </div>
     </section>
   )
--- components/Layout.js
+++ components/Layout.js
@@ -5,8 +5,8 @@ const Layout = ({children}) => {
     <div>
       <Head>
         <link href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" rel="stylesheet" />
-        <link rel='icon' type='image/png' href='/logo.jpeg' />
-        <link rel="apple-touch-icon" href="/logo.jpeg" />
+        <link rel='icon' type='image/png' href='/logoWhite.jpg' />
+        <link rel="apple-touch-icon" href="/logoWhite.jpg" />
         <title>WasteBridge Protocol</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
